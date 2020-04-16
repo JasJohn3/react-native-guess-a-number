a3e32ce6c448ddc0888a3dfb61ae1c6514a8a819
Jason Lane Johnson <jasjohn3@uat.edu>
2020-04-16 16:56:44 -0700

    Game Loop Complete

---
+++ screens/GameOverScreen.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import Colors from "../constants/colors";
+const GameOverScreen = (props) => {
+  return (
+    <View style={styles.screen}>
+      <Text style={styles.text}>You Won!</Text>
+      <Text style={styles.text}>Thank you for playing!</Text>
+      <Text style={styles.text}>Number of Rounds: {props.roundsNumber}</Text>
+      <Text style={styles.text}>Correct Number: {props.userNumber}</Text>
+      <Button
+        title="NEW GAME!"
+        style={styles.exitButton}
+        onPress={props.onRestart}
+      />
+    </View>
+  );
+};
+
+export default GameOverScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.accent,
+  },
+  text: {
+    color: Colors.fontPrimary,
+    backgroundColor: Colors.primary,
+    fontSize: 22,
+    padding: 10,
+    margin: 10,
+  },
+  exitButton: {
+    flex: 1,
+    color: Colors.primary,
+    elevation: 5,
+    width: 100,
+  },
+});
